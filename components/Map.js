44e14893451629dde4a00c5eb98b7e19484dc362
Adokiye <gorgeceo@gmail.com>
2020-02-25 17:02:08 +0100

    Multiple changes

--- components/Map.js
+++ components/Map.js
@@ -159,12 +159,14 @@ class reduxMap extends Component {
       );
     }
     if (params && params.invite) {
+      console.log(params.start_location)
+      console.log(JSON.log(params.start_location))
       params.invite = false;
       this.setState({ invited_location: params.start_location });
     }
     if (params && params.chosen) {
       params.chosen = false;
-      this.setState({ reciever: params.receiver }, () =>
+      this.setState({ receiver: params.receiver }, () =>
         this.setState({ find: false })
       );
     }
